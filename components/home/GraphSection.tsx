211302e4b78981a3b5902062cd36c8dc7c64f432
moo1677 <lgm04@naver.com>
2026-01-12 22:50:32 +0900

    chore: 텍스트 편집

--- components/home/GraphSection.tsx
+++ components/home/GraphSection.tsx
@@ -7,7 +7,7 @@ interface GraphSectionProps {
 const GraphSection = ({ className }: GraphSectionProps) => {
   return (
     <div className={className}>
-      <Card title="학습 기록">대충 잔디그래프</Card>
+      <Card title="학습 기록">잔디그래프 구현 예정</Card>
     </div>
   );
 };
