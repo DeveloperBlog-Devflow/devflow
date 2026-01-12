df267331f38c7ad2f6d6ef297fee75a8de66323f
moo1677 <lgm04@naver.com>
2026-01-12 20:02:52 +0900

    style: 랜딩페이지 스타일 변경

--- components/landing/LandingPreview.tsx
+++ components/landing/LandingPreview.tsx
@@ -4,19 +4,16 @@ import FeatureContent from '@/components/landing/FeatureContent';
 const LandingPreview = () => {
   return (
     <div className="mt-30 flex h-full flex-col items-center justify-center space-y-3 pb-50 text-center">
-      <p className="bg-surface-alt rounded-mg mt-16 rounded-lg p-1 px-4 text-xs">
-        🤔 왜 DevFlow를 사용할까요?
-      </p>
       <h1 className="text-4xl font-bold">체계적인 학습으로</h1>
       <h1 className="text-primary text-4xl font-bold">더 빠른 성장</h1>
-      <div className="mt-10 flex gap-50">
+      <div className="mx-20 mt-10 flex flex-col items-center gap-10 lg:flex-row lg:items-start lg:gap-50">
         <FeatureContent />
         <Image
           src="/PreviewImage.png"
           alt="DevFlow 미리보기"
-          className="w-full object-cover"
-          width={430}
-          height={232}
+          width={720}
+          height={400}
+          className="w-full max-w-[480px] min-w-[360px] flex-shrink-0 object-contain md:max-w-[560px] lg:max-w-[640px] xl:max-w-[720px]"
         />
       </div>
     </div>
