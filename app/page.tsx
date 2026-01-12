a85363f14d3f6f7ff801e647dbdba0009db613dd
김동은 <ehddms0517@naver.com>
2026-01-12 19:41:51 +0900

    refactor: 레이아웃 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import LandingHeader from '@/components/landing/LandingHeader';
-import HeroSection from '@/components/landing/HeroSection';
-import FeatureList from '@/components/landing/FeatureList';
-import LandingPreview from '@/components/landing/LandingPreview';
-
 export default function Home() {
-  return (
-    <div className="bg-background h-full space-y-60">
-      <LandingHeader />
-      <HeroSection />
-      <FeatureList />
-      <LandingPreview />
-    </div>
-  );
+  return <div>home</div>;
 }
