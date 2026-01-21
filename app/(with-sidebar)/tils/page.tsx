2b947dbbba07ff16272d047f815371d6104fa6c8
김동은 <ehddms0517@naver.com>
2026-01-22 04:02:46 +0900

    feat: Header 및 SearchBar 구현

--- app/(with-sidebar)/tils/page.tsx
+++ app/(with-sidebar)/tils/page.tsx
@@ -1,13 +1,15 @@
 import PageHeader from '@/components/common/PageHeader';
+import ToolBar from '@/components/tils/ToolBar';
 
 const Page = () => {
   return (
-    <div className="bg-background min-h-screen p-11">
+    <div className="bg-background min-h-screen px-30 py-11">
       <PageHeader
         title="일지"
         highlight="관리하기"
         description="작성한 일지를 관리해보세요"
       />
+      <ToolBar />
     </div>
   );
 };
