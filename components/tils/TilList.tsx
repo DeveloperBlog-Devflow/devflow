d55548b2f6b7a8b67e3aae8bf0b1498cb7302dd1
moo1677 <lgm04@naver.com>
2026-01-22 22:44:35 +0900

    feat: TIL 삭제 구현

--- components/tils/TilList.tsx
+++ components/tils/TilList.tsx
@@ -1,17 +1,18 @@
-'use client'
+'use client';
 
 import type { TilItem } from '@/types/til';
 import TilCard from './TilCard';
 
 type Props = {
   items: TilItem[];
+  onDelete: (id: string) => Promise<void>;
 };
 
-const TilList = ({ items }: Props) => {
+const TilList = ({ items, onDelete }: Props) => {
   return (
     <div className="space-y-8">
       {items.map((it) => (
-        <TilCard item={it} key={it.id} />
+        <TilCard item={it} key={it.id} onDelete={onDelete} />
       ))}
     </div>
   );
