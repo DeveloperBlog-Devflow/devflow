672d6e6d6fe7658581da99102080afa7f196cc77
moo1677 <lgm04@naver.com>
2026-01-28 16:12:06 +0900

    feat: todo 수정, 삭제 구현

--- services/home/todoService.service.ts
+++ services/home/todoService.service.ts
@@ -7,40 +7,75 @@ import {
   query,
   Timestamp,
   updateDoc,
+  deleteDoc,
+  where,
 } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
+import { dateKeyKST } from '@/lib/date';
 
 // 할 일 데이터 타입
 export interface Todo {
   id: string;
   text: string;
   isChecked: boolean;
-  createAt: Date;
+  createdAt: Date;
 }
 
+// 0시 초기화
+
 // 1. 할 일 목록 가져오기
-export const fetchTodos = async (uid: string): Promise<Todo[]> => {
+export const fetchTodos = async (
+  uid: string,
+  dateKey: string
+): Promise<Todo[]> => {
   const todosCollectionPath = collection(db, 'users', uid, 'todos');
-  const q = query(todosCollectionPath, orderBy('createAt', 'asc'));
+  const q = query(
+    todosCollectionPath,
+    where('dateKey', '==', dateKey),
+    orderBy('createdAt', 'asc')
+  );
   const snapshot = await getDocs(q);
 
-  return snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-    createAt: doc.data().createAt.toDate() ?? new Date(),
-  })) as Todo[];
-};
-
-// 2. 할 일 추가하기
-export const AddTodo = async (uid: string, text: string) => {
-  await addDoc(collection(db, 'users', uid, 'todos'), {
-    text,
-    isChecked: false,
-    createAt: Timestamp.now(),
+  return snapshot.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      id: doc.id,
+      text: data.text,
+      isChecked: !!data.isChecked,
+      createdAt: doc.data().createdAt?.toDate?.() ?? new Date(),
+    };
   });
 };
 
-// 3. 완료 상태 토글
+// 2. 할 일 추가하기
+export const addTodo = async (uid: string, text: string) => {
+  await addDoc(collection(db, 'users', uid, 'todos'), {
+    text,
+    isChecked: false,
+    dateKey: dateKeyKST(),
+    createdAt: Timestamp.now(),
+  });
+};
+
+// 3. 할 일 삭제하기
+export const deleteTodo = async (uid: string, todoId: string) => {
+  const todoRef = doc(db, 'users', uid, 'todos', todoId);
+  await deleteDoc(todoRef);
+};
+
+// 4. 할 일 수정하기
+export const updateTodoText = async (
+  uid: string,
+  todoId: string,
+  text: string
+) => {
+  const todoRef = doc(db, 'users', uid, 'todos', todoId);
+  await updateDoc(todoRef, {
+    text,
+  });
+};
+
+// 5. 완료 상태 토글
 export const toggleTodoStatus = async (
   uid: string,
   todoId: string,
